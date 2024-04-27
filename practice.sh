8cd8e9052bf645fb6eb0e9b32ecd293e9bcd0a32
Pretentious-hacker <abhistaryan16@gmail.com>
2024-04-27 22:19:52 +0530

    Blindin

--- practice.sh
+++ practice.sh
@@ -3,4 +3,18 @@
 # do
 # echo "fuck your mom ${i} times"
 # done
+# myvar=$1
+# for i in {0..${myvar}};
+# do
+# echo $1
+# echo "Hello Mother Fuckers $i times"
+# done
 
+for i in file_{1..69}
+do 
+    touch ./touchingrass/$i
+    for ((j=0; j<=$i; j++))
+    do
+    echo "Fuck your mother $j times" >> ./touchingrass/$i
+    done
+done
